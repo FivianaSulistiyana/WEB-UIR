4c9e7c99a5c83614013642df9233f0e7cb1fa32a
FivianaSulistiyana <fivianasulistiyana05@gmail.com>
2021-12-30 16:56:52 +1400

    Latihan 5.2: Melakukan operasi sederhana dengan operator aritmatika

---
+++ learn.js
@@ -0,0 +1,28 @@
+
+//membuat variable
+    let myNumber=16;
+    let myString="Hello"
+    let myBoolean=true
+    const phi=3.14
+    //menampilkan informasi ke console
+    console.log(myNumber)
+    console.log(myString)
+    console.log(myBoolean)
+    console.log(phi)
+    //menampilkan informasi dalam bentuk alert pada window
+    window.alert("Angka favorit saya adalah:"+myNumber)
+    window.alert('Angka favorit saya adalah:'+myNumber)
+    window.alert(`Angka favorit saya adalah:${myNumber}`)
+
+    let x = 8
+let y = 4
+console.log(x, y)
+console.log('x + y =', x + y)
+console.log('x - y =', x - y)
+console.log('x * y =', x * y)
+console.log('x / y =', x / y)
+console.log('x % y =', x % y)
+console.log('x++ =', x++)
+console.log('++x =', ++x)
+console.log('x-- =', x--)
+console.log('--x =', --x)
