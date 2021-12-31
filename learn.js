47ab298adda334f1aac850546da65822da412992
FivianaSulistiyana <fivianasulistiyana05@gmail.com>
2022-01-01 10:55:03 +1400

    Latihan 5.8: Anomalia pada javascript

--- learn.js
+++ learn.js
@@ -125,3 +125,16 @@ console.log(person[ "umur"]);
 console.log(person[ "jurusan"]);
 console.log(person["fullName"]());
 
+//anomalia pada javascript
+console.log(0.1 + 0.2 == 0.3)
+console.log(1 + "1")
+console.log(1 - "1")
+console.log(1 < 2 < 3)
+console.log(3 > 2 > 1)
+console.log(null == 0)
+console.log(null > 0)
+console.log(null >=0)
+
+
+
+
